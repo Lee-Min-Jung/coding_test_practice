c011de81cef380fa4b559ef064103730cf52a6c3
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-02-09 16:41:33 +0900

    solveA : 프로그래머스 고득점 Kit - 자릿수 더하기
    - 형 변환 없이 수학적으로 푸는 방법도 있음

---
+++ 프로그래머스/Level1/자릿수 더하기/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        String[] n_arr = String.valueOf(n).split("");
+
+        for(int i = 0; i<n_arr.length; i++){
+            answer += Integer.parseInt(n_arr[i]);
+        }
+
+        return answer;
+    }
+}
