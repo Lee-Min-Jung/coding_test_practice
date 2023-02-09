031ed4ca8008978e55d5924ee7329cf7d8b92f31
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-02-09 15:06:39 +0900

    solveA@ : 프로그래머스 고득점 Kit - 같은 숫자는 싫어
    - 그냥 일반 리스트로 푸는 방법도 있음

---
+++ 프로그래머스/코딩테스트 고득점 Kit/같은 숫자는 싫어/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        // 스택 만들어서 arr에 있는 거 집어 넣기
+        // 집어 넣을 때 그 전에 거 빼서 집어 넣는 거랑 같으면 안 넣고 다르면 넣기
+        // 다 넣은 후에 빼서 배열에 넣기
+
+        Stack<Integer> s = new Stack<Integer>();
+        for(int num : arr){
+            if(s.isEmpty()){
+                s.push(num);
+            }else if(s.peek() != num){
+                s.push(num);
+            }
+        }
+
+        int[] answer = new int[s.size()];
+
+        for(int i = answer.length-1; i>=0; i--){
+            answer[i] = s.pop();
+        }
+
+        return answer;
+    }
+}
