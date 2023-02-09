f2cb311986f7784b423656895d6b2a387fa6ad07
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-02-09 17:25:33 +0900

    solve : Level1 - 문자열 내 p와 y의 개수

---
+++ 프로그래머스/Level1/문자열 내 p와 y의 개수/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    boolean solution(String s) {
+        String[] s_arr = s.split("");
+        int p_count = 0;
+        int y_count = 0;
+
+        for(int i = 0; i<s_arr.length; i++){
+            if(s_arr[i].equals("p") || s_arr[i].equals("P")){
+                p_count += 1;
+            }
+            if(s_arr[i].equals("y") || s_arr[i].equals("Y")){
+                y_count += 1;
+            }
+        }
+
+        return p_count == y_count ? true : false;
+
+    }
+}
