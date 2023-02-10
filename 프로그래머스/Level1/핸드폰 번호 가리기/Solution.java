025415470e1404053ae896c6d06ac8564b014065
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-02-10 16:58:39 +0900

    solve@ : Level1 - 핸드폰 번호 가리기
    - String join 메서드 사용법 -> String.join("", 배열이름);

---
+++ 프로그래머스/Level1/핸드폰 번호 가리기/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String phone_number) {
+        // 길이 - 4의 인덱스부터 끝까지를 바꾸면 됨
+
+        int index = phone_number.length() - 4;
+        String[] phone_number_arr = phone_number.split("");
+
+        for(int i = 0; i<phone_number_arr.length; i++){
+            if(i < index){
+                phone_number_arr[i] = "*";
+            }
+        }
+
+        return String.join("",phone_number_arr);
+    }
+}
