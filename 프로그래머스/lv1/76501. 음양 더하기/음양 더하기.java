0bfac55204d2b7e9c62ab5a1f03fcf5d6e5d0f0a
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-02-15 14:56:16 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i = 0; i<signs.length; i++){
+            int num = 0;
+            if(signs[i]){
+                num = absolutes[i];
+            }else{
+                num = -absolutes[i];
+            }
+            answer += num;
+        }
+        return answer;
+    }
+}
