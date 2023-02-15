1eeb9bec1e27ecb6d3d20ff4e9f7679292d4c62d
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-02-15 14:44:08 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = 1;
+                
+        while(num <= n){
+            if(n % num == 0){
+                answer += num;
+            }
+            num++;
+        }
+        
+        return answer;
+    }
+}
