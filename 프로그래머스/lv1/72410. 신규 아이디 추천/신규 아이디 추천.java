c7a58204aeccbe4b8368ed3fcb223cd9b33fb6e0
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-02-15 14:50:38 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.82 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String new_id) {
+        // 1단계
+        new_id = new_id.toLowerCase();
+        // 2단계
+        new_id = new_id.replaceAll("[^-.\\w]","");
+        // 3단계
+        new_id = new_id.replaceAll("\\.{2,}","\\.");
+        // 4단계
+        new_id = new_id.replaceAll("^\\.", "");
+        new_id = new_id.replaceAll("\\.$", "");
+        // 5단계
+        new_id = new_id.equals("") ? "a" : new_id;
+        // 6단계
+        if(new_id.length() >= 16){
+            new_id = new_id.substring(0,15);
+            new_id = new_id.replaceAll("\\.$", "");
+        }
+        // 7단계
+        if(new_id.length() <= 2){
+            String last = String.valueOf(new_id.charAt(new_id.length()-1));
+            while(new_id.length() != 3){
+                new_id += last;
+            }
+        }
+        return new_id;
+        
+    }
+}
