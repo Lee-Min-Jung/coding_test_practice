6d2d66918147ced8a9c9313f0f49db2b443e37c2
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-02-15 14:55:14 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long num2 = (long)num;
+        
+        while(answer <=500 && num2 != 1){
+            if(num2 % 2 == 0){
+                num2 = num2 / 2;
+                answer ++;
+            }else if(num2 % 2 == 1){
+                num2 = num2 * 3 + 1;
+                answer ++;
+            }else if(num2 == 1){
+                answer = 0;
+                break;
+            }
+        }
+        if(answer > 500){
+            return -1;
+        }
+        
+        
+        
+        return answer;
+    }
+}
