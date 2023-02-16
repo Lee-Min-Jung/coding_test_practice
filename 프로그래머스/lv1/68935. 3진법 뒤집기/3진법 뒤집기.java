c7e12a9f122dd216eeb287617cece4a7c20a13b9
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-02-16 15:08:35 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.17 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,17 +2,14 @@ class Solution {
     public int solution(int n) {
         StringBuilder threeMethod = new StringBuilder();
         int answer = 0;
-        // 앞 뒤 반전
+        // 3진법으로
         while(n != 0){
             threeMethod.append(n % 3);
             n = n / 3;
         }
-        
+        System.out.println(threeMethod.reverse().toString());
         //10진법으로
-        String[] threeMethodArr = threeMethod.reverse().toString().split("");
-        for(int i = 0; i<threeMethodArr.length; i++){
-            answer += Integer.parseInt(threeMethodArr[i]) * Math.pow(3, i);
-        }
+        answer = Integer.parseInt(threeMethod.reverse().toString(), 3);
         
         
         return answer;
