719433ab4a1f37e3a30c9c9b8ac1b6bc496b2594
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-02-16 14:48:07 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.08 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,30 +1,14 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        int divisor = 1;
-        int small = n < m ? n : m;
-        int big = n > m ? n : m;
-        int maxYackSu = 0;
-        int minBaeSu = 0;
         
-        // 최대공약수
-        while(divisor <= small){
-            if(n % divisor == 0 && m % divisor == 0){
-                maxYackSu = divisor;
+        for(int i = 1; i<=(int)Math.min(n,m); i++){
+            if(n % i == 0 && m % i == 0){
+                answer[0] = i;
+                answer[1] = n*m / i;
             }
-            divisor++;
-        }
-        
-        answer[0] = maxYackSu;
-        
-        // 최소공배수
-        if(maxYackSu == 1){
-            answer[1] = n * m;
-        }else{
-            answer[1] = (n / maxYackSu) * (m / maxYackSu) * maxYackSu;
         }
         
         return answer;
-        
     }
 }
