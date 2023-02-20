02a65683fa7e6b6f0d58ae01d84448c3876541a8
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-02-20 16:17:07 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] number) {
+        int sum = 0;
+        int answer = 0;
+        
+        for(int i = 0; i<number.length; i++){
+            for(int j = i+1; j<number.length; j++){
+                for(int k = j+1; k<number.length; k++){
+                    sum = number[i] + number[j] + number[k];
+                    if(sum == 0){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+        
+        
+        
+    }
+}
