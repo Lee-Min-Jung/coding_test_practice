90500721f12950537cc993fb2b655f25efedf104
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-02-21 17:54:49 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 9.21 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,7 +3,9 @@ class Solution {
         StringBuilder foodOrder = new StringBuilder();
         
         for(int i = 1; i<food.length; i++){
-            foodOrder.append(String.valueOf(i).repeat(food[i]/2));
+            for(int j = 0; j<food[i]/2; j++){
+                foodOrder.append(String.valueOf(i));
+            }
         }
         
         return foodOrder.toString() + "0" + foodOrder.reverse().toString();
