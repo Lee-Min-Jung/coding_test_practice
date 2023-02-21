5ad46d2c087d13ab5cb40699b31bebc9195b7b1c
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-02-21 16:15:56 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,39 +1,16 @@
 class Solution {
     public String solution(int a, int b) {
+        String[] dayStr = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
         int[] monthDay = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String answer = getDay(a, b, monthDay);
-        return answer;
+        int sumDays = 0;
+        
+        for(int i = 0; i<a-1; i++){
+            sumDays += monthDay[i];
+        }
+        sumDays += b-1;
+        
+        return dayStr[sumDays % 7];
     }
     
-    public String getDay(int a, int b, int[] monthDay){
-        int sumDay = 0;
-        int left = 0;
-        if(a != 1){
-            for(int i = 0; i<a-1; i++){
-                sumDay += monthDay[i];
-            }
-        }
-        sumDay += b-1;
-        left = sumDay % 7;
-        
-        switch(left){
-            case 1:
-                return "SAT";
-            case 2:
-                return "SUN";
-            case 3:
-                return "MON";
-            case 4:
-                return "TUE";
-            case 5:
-                return "WED";
-            case 6:
-                return "THU";
-            case 0:
-                return "FRI";
-            default:
-                return "";
-        }
-        
-    }
+    
 }
