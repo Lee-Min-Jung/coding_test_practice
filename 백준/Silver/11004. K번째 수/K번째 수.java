2a4b47d25a4c61cdd68a115414c578a9cacaa8d7
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-03-30 16:26:18 +0900

    [Silver V] Title: K번째 수, Time: 1780 ms, Memory: 371100 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -13,11 +13,46 @@ public class Main {
         for(int i = 0; i<N; i++){
             nums[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(nums);
+        quickSort(nums, 0, N-1);
         System.out.println(nums[K-1]);
 
     }
+    private static void quickSort(int[] nums, int l, int r){
+        if(l >= r){
+            return;
+        }
+        int pivot = partition(nums, l, r);
+        quickSort(nums, l, pivot);
+        quickSort(nums, pivot + 1, r);
+    }
 
+    private static int partition(int[] nums, int l, int r){
+        int left = l-1;
+        int right = r+1;
+        int pivot = nums[(l+r)/2];
+
+        while(true){
+            do{
+                left++;
+            }while(nums[left] < pivot);
+            
+            do{
+                right--;
+            }while(nums[right] > pivot && left <= right);
+            
+            if(left >= right){
+                return right;
+            }
+            
+            swap(nums, left, right);
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
 }
 
