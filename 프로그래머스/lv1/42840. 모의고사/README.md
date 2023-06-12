fb2950054c892285b3c696f9cf77e9b0226f69d2
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-12 15:02:32 +0900

    [level 1] Title: 모의고사, Time: 1.26 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/README.md
+++ 프로그래머스/lv1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#qna) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 0.73 ms
+메모리: 94.8 MB, 시간: 1.26 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,33 +1,70 @@
+// 1번 수포자: 1 2 3 4 5 반복, 5개
+// 2번 수포자: 2 1 2 3 2 4 2 5 반복, 8개
+// 3번 수포자: 3 3 1 1 2 2 4 4 5 5 반복, 10개
+// answers의 개수가 곧 문제의 개수
+
+// answers를 돌면서 각 수포자 배열과 비교해서 정답 개수 구하기
+    // 각 수포자 배열은 계속 반복되니 비교할 때 나머지 연산 이용
+    // answers의 인덱스 % 각 수포자 배열 길이
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] first = {1,2,3,4,5}; // 5개씩 반복
-        int[] second = {2,1,2,3,2,4,2,5}; // 8개씩 반복
-        int[] third = {3,3,1,1,2,2,4,4,5,5}; // 10개씩 반복
-        int[] score = {0,0,0}; // 각 수포자들의 점수
+        // 각 수포자 배열
+        int[] first = {1,2,3,4,5};
+        int[] second = {2,1,2,3,2,4,2,5};
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
+      
+        int[] score = new int[3];
         
-        // 수포자들의 점수 계산
-        for(int i=0; i<answers.length; i++) {
-            if(answers[i] == first[i%5]) score[0]++;
-            if(answers[i] == second[i%8]) score[1]++;
-            if(answers[i] == third[i%10]) score[2]++;
+        for(int i = 0; i<answers.length; i++){
+            if(answers[i] == first[i%first.length]){
+                score[0]++;
+            }
+            if(answers[i] == second[i%second.length]){
+                score[1]++;
+            }
+            if(answers[i] == third[i%third.length]){
+                score[2]++;
+            }
+            
         }
         
-        // 최대 점수 구하기
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
-        
-        System.out.println(max);
-        
-        // 최대 점수를 가진 수포자 리스트 생성
-        List<Integer> answ = new ArrayList<Integer>();
-        for(int i=0; i<score.length; i++) if(max == score[i]) answ.add(i+1);
-        
-        int[] answer = new int[answ.size()];
-        for(int i=0; i<answ.size(); i++){
-            answer[i] = answ.get(i);
+        // 테스트 확인
+        for(int n : score){
+            System.out.println(n);
         }
-
-        return answer;
+        
+        // 많이 맞힌 사람 찾기
+        int max = score[0];
+        List<Integer> answer = new ArrayList<Integer>();
+        
+        for(int i = 0; i<3; i++){
+            if(score[i] >= max & score[i] != 0){
+                max = score[i];
+            }
+        }
+        
+        for(int i = 0; i<3; i++){
+            if(score[i] == max){
+                answer.add(i+1);
+            }
+        }
+        
+        Collections.sort(answer);
+        
+        // 결과 리턴
+        int[] answerArr = new int[answer.size()];
+    
+        if(answer.isEmpty()){
+            return answerArr;
+        }else{
+            for(int i = 0; i<answer.size(); i++){
+                answerArr[i] = answer.get(i);
+            }
+        }
+        return answerArr;
+        
+     
     }
 }
