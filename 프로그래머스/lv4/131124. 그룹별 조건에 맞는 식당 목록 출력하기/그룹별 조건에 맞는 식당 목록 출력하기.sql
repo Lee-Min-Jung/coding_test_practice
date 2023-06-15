d006352bdbe374dec2319ca69acd24b76bab49c7
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-15 22:07:21 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,34 +1,8 @@
-# SELECT m.member_name, r.review_text, date_format(r.review_date, "%Y-%m-%d")
-# FROM member_profile m
-# -- 프로필과 리뷰 정보 다 필요해서 join
-# INNER JOIN (
-#     SELECT *
-#     FROM rest_review 
 
-#     -- 가장 많은 리뷰 수를 가지고 있는 멤버들만 뽑음
-#     WHERE member_id in (
-#         SELECT member_id 
-#         FROM rest_review
-#         GROUP BY member_id
-
-#         -- 멤버 아이디 별로 그룹지었을 때, 그 리뷰 개수가 최대인 멤버만 뽑기
-#         HAVING count(*) = (
-
-#             -- 가장 많은 리뷰가 몇 개인지 뽑기
-#             SELECT count(*)
-#             FROM rest_review
-#             GROUP BY member_id
-#             ORDER BY count(*) desc
-#             LIMIT 1
-#         )
-#     )
-# ) r
-# ON m.member_id = r.member_id
-# ORDER BY r.review_date, r.review_text
 
 SELECT P.MEMBER_NAME, N.REVIEW_TEXT, DATE_FORMAT(N.REVIEW_DATE, "%Y-%m-%d") AS REVIEW_DATE
 FROM MEMBER_PROFILE P
-INNER JOIN(
+RIGHT JOIN(
     SELECT *
     FROM REST_REVIEW R2
     WHERE R2.MEMBER_ID IN (
