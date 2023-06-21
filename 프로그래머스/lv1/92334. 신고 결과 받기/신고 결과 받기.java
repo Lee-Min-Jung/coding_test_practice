0e2fcf70ada85c284ae8958f4e4bf6ef050c938a
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-21 18:02:49 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.25 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,42 +1,47 @@
+// report를 돌면서 신고 내역 저장
+    // hashMap에다가 신고자 : 신고 당한 사람 저장
+        // hashMap<String, HashSet<String>>
+// report 돌면서 신고 받은 사람 횟수 저장
+    // HashMao<String, Integer>
 import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        // report를 돌면서 누가 누구를 신고했는지 정리
-            // map, 신고한 사람: 신고당한 사람 list
-        // report를 돌면서 신고당한 아이디 별로 신고당한 횟수 정리
-            // map, 신고당한 아이디 : 신고당한 횟수
-        // id_list를 돌면서 reportMap에서 찾고 해당 map의 set을 돌면서 set원소를 reportedCount에서 찾아서 k보다 크거나 같으면 1 증가
+        // 신고 내역 
+        HashMap<String, HashSet<String>> reportStatus = new HashMap<String, HashSet<String>>();
+        // 신고 횟수
+        HashMap<String, Integer> reportCount = new HashMap<String, Integer>();
+        // 정지자
+        ArrayList<String> cancel = new ArrayList<String>();
         
-        HashMap<String, HashSet<String>> reportMap = new HashMap<String, HashSet<String>>();
-        HashMap<String, Integer> reportedCount = new HashMap<String, Integer>();
-        HashSet<String> repotedIdSet = new HashSet<String>();
-        int[] answer = new int[id_list.length];
-        
-        for(String rep : report){
-            String repoter = rep.split(" ")[0];
-            String repoted = rep.split(" ")[1];
-            repotedIdSet = reportMap.getOrDefault(repoter, new HashSet<String>());
-            repotedIdSet.add(repoted);
-            reportMap.put(repoter, repotedIdSet);
+        // report 돌면서 신고 내역 저장
+        for(int i = 0; i<report.length; i++){
+            String reporter = report[i].split(" ")[0];
+            String reported = report[i].split(" ")[1];
+            
+            HashSet<String> hs = reportStatus.getOrDefault(reporter, new HashSet<String>());
+            hs.add(reported);
+            reportStatus.put(reporter, hs);
         }
         
-        for(String key : reportMap.keySet()){
-            for(String repoted : reportMap.get(key)){
-                reportedCount.put(repoted, reportedCount.getOrDefault(repoted, 0)+1);
+        // reportStatus 돌면서 신고 횟수 저장
+        for(String key : reportStatus.keySet()){
+            for(String reported : reportStatus.get(key)){
+                reportCount.put(reported, reportCount.getOrDefault(reported, 0)+1);
             }
         }
         
+        // id_list돌면서 각 아이디별로 메일 받는 횟수 구하기
+        int[] answer = new int[id_list.length];
         for(int i = 0; i<id_list.length; i++){
             int count = 0;
-            for(String repoted : reportMap.getOrDefault(id_list[i], new HashSet<String>())){
-                if(reportedCount.get(repoted) >= k){
+            for(String key : reportStatus.getOrDefault(id_list[i], new HashSet<String>())){
+                if(reportCount.get(key) >= k){
                     count++;
                 }
             }
             answer[i] = count;
         }
-        
         
         return answer;
         
