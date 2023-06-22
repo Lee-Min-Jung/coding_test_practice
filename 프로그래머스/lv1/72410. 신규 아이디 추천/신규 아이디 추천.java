52dc8cbb377e81d9b0c2b94529006a87f45a6bab
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-22 16:09:46 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.10 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,29 +1,48 @@
+// 1단계: 대문자를 소문자로 치환
+// 2단계: 알파벳 소문자, 숫자,-, _, . 만 남기기
+// 3단계: 마침표 2번 이상인 부분 1번으로 치환
+// 4단계: 마침표가 처음이나 끝이면 없애기
+// 5단계: 빈 문자열이면 a 대입
+// 6단계: 16자 이상이면 15개만 남기기. 만약 제거 후 마침표가 맨 마지막이면 마침표 제거
+// 7단계: 길이가 2 이하라면 마지막 문자를 길이가 3될 때까지 반복
+
 class Solution {
     public String solution(String new_id) {
         // 1단계
-        new_id = new_id.toLowerCase();
+        String answer = new_id.toLowerCase();
+        System.out.println(answer);
         // 2단계
-        new_id = new_id.replaceAll("[^-.\\w]","");
+        answer = answer.replaceAll("[^-.\\w]", "");
+        System.out.println(answer);
         // 3단계
-        new_id = new_id.replaceAll("\\.{2,}","\\.");
+        answer = answer.replaceAll("\\.{2,}", "\\.");
+        System.out.println(answer);
         // 4단계
-        new_id = new_id.replaceAll("^\\.", "");
-        new_id = new_id.replaceAll("\\.$", "");
+        answer = answer.replaceAll("^(\\.)", "");
+        answer = answer.replaceAll("(\\.)$", "");
+        System.out.println(answer);
         // 5단계
-        new_id = new_id.equals("") ? "a" : new_id;
-        // 6단계
-        if(new_id.length() >= 16){
-            new_id = new_id.substring(0,15);
-            new_id = new_id.replaceAll("\\.$", "");
+        if(answer.equals("")){
+            answer += "a";
         }
+        System.out.println(answer);
+        // 6단계
+        if(answer.length() >= 16){
+            answer = answer.substring(0, 15);
+        }
+        answer = answer.replaceAll("(\\.)$", "");
+        System.out.println(answer);
         // 7단계
-        if(new_id.length() <= 2){
-            String last = String.valueOf(new_id.charAt(new_id.length()-1));
-            while(new_id.length() != 3){
-                new_id += last;
+        if(answer.length() <= 2){
+            String lastStr = answer.split("")[answer.length()-1];
+            while(answer.length() !=3){
+                answer += lastStr;
             }
         }
-        return new_id;
         
+        
+      
+        
+        return answer;
     }
 }
