4993a7501103207a9778a395618e85c40e2140cf
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-22 11:33:41 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 2.11 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,54 +1,84 @@
+// today: 오늘 날짜
+// terms: 약관 별 유효기간
+// privacies: 개인정보 수집 일자와 약관 종류
+
+// terms를 돌면서 약관별 유효기간을 map으로 저장한다
+// privacies를 돈다
+    // 일자와 약정 유형을 뽑아 낸다
+    // map에서 약정 유형에 따른 월 수를 구한다. 
+    // 일자에 월을 더한 날짜가 오늘보다 나중이면 파기해야 한다
+    
 import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        // terms를 돌면서 각 약정 별 기간을 map에 저장
-        // privacies를 돈다
-            // 약정 종류와 기간을 map에서 찾는다
-            // 날짜에 찾은 기간을 더한 후 올바른 날짜로 만든다
-                // 
-            // 날짜와 today를 비교해서 today를 지나지 않았으면 index+1을 답에 추가한다
-        
-        List<Integer> answerList = new ArrayList<Integer>();
-        HashMap<String, Integer> termPeriod = new HashMap<String, Integer>();
-        for(String term : terms){
-            termPeriod.put(term.split(" ")[0], Integer.parseInt(term.split(" ")[1]));
+        // terms 돌면서 약관별 유효기간 저장
+        HashMap<String, Integer> termMap = new HashMap<String, Integer>();
+        for(String str : terms){
+            String type = str.split(" ")[0];
+            int month = Integer.parseInt(str.split(" ")[1]);
+            termMap.put(type, month);
         }
+        // 오늘 날짜
+        int tYear = Integer.parseInt(today.split("\\.")[0]);
+        int tMonth = Integer.parseInt(today.split("\\.")[1]);
+        int tDay = Integer.parseInt(today.split("\\.")[2]);
         
+        // 정답 답을 리스트
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        
+        // privacies를 돌기
         for(int i = 0; i<privacies.length; i++){
             String date = privacies[i].split(" ")[0];
             String type = privacies[i].split(" ")[1];
-            int tYear = Integer.parseInt(today.split("\\.")[0]);
-            int tMonth = Integer.parseInt(today.split("\\.")[1]);
-            int tDay = Integer.parseInt(today.split("\\.")[2]);
             int year = Integer.parseInt(date.split("\\.")[0]);
             int month = Integer.parseInt(date.split("\\.")[1]);
             int day = Integer.parseInt(date.split("\\.")[2]);
-            int period = termPeriod.get(type);
+            int termPeriod = termMap.get(type);
             
-            if(period % 12 == 0){
-                year += period / 12;
-            }else if((period + month) > 12){
-                year += (period + month) / 12;
-                month = (period + month) % 12;
+            int plusYear = 0;
+            int plusMonth = 0;
+            // termPeriod가 12 넘는 경우
+            if(termPeriod > 12){    
+                plusYear = termPeriod / 12;
+                plusMonth = termPeriod % 12;
             }else{
-                month += period;
+                plusYear = 0;
+                plusMonth = termPeriod;
             }
             
-            Date f = new Date(year, month, day);
-            Date t = new Date(tYear, tMonth, tDay);
-            if(t.after(f)){
-                answerList.add(i+1);
-            }else if(t.equals(f)){
-                answerList.add(i+1);
+            // privacies에 plus 월 더하기
+            month = month + plusMonth;
+            
+            // 만약 month 12보다 큰 경우
+            if(month > 12){
+                year += month / 12;
+                month = month % 12;
             }
             
-            
+            // privacies에 plus 연 더하기
+            year = year + plusYear;
+        
+            // 오늘 날짜랑 비교하기
+            if(tYear > year){
+                result.add(i+1);
+            }else if(tYear == year && tMonth > month){
+                result.add(i+1);
+            }else if(tYear == year && tMonth == month && tDay > day){
+                result.add(i+1);
+            }else if(tYear == year && tMonth == month && tDay == day){
+                result.add(i+1);
+            }
         }
-        int[] answer = new int[answerList.size()];
+        // 정답 배열
+        int[] answer = new int[result.size()];
+        
+        // 정답 배열에 담기
         for(int i = 0; i<answer.length; i++){
-            answer[i] = answerList.get(i);
+            answer[i] = result.get(i);
         }
+        
         return answer;
+        
     }
 }
