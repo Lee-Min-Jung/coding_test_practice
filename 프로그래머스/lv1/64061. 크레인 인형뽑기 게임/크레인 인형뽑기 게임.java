a10b90492906e672a26fc011291079b36baf24b0
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-23 11:28:56 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.43 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,37 +1,66 @@
+// board: 게임 화면의 격자의 상태가 담긴 2차원 배열 
+// moves: 인형을 집기 위해 크레인을 작동시킨 위치
+// 크레인을 모두 작동시킨 후 터트려져 사라진 인형의 개수
+
+
+// board를 돌면서 0제외 숫자들만 따로 배열을 만든다
+// moves를 돌면서
+    // moves의 값에 맞는 걸 위에서 만든 배열에서 찾아서 제거
+    // 위에서 찾은 값을 스택에 집어 넣기
+    // 스택에 연속된 값 있는지 확인해서 연속된 값이면 제거하고 개수 증가
 import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        // moves를 돌면서 moves의 원소 하나를 가져와 해당 원소를 행으로 하는 것 중 0아닌 걸 찾아서 저장후 해당 원소 0으로 만들기
-        // 스택에서 하나 꺼낸 후 위에서 저장한 것과 비교해서 같으면 답에 2 더하고 제거, 같지 않으면 스택에 추가
-        Stack<Integer> dollStack = new Stack<Integer>();
-        int answer = 0;
-        
-        for(int i = 0; i<moves.length; i++){
-            int col = moves[i]-1;
-            for(int j = 0; j<board.length; j++){
-                if(board[j][col] == 0){
+        // board 돌면서 0제외 숫자들만의 배열 만들기
+        List[] realBoard = new ArrayList[board.length+1];
+        for(int i = 0; i<realBoard.length; i++){
+            realBoard[i] = new ArrayList<Integer>();
+        }
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board[i].length; j++){
+                if(board[i][j] == 0){
                     continue;
                 }else{
-                    if(!dollStack.isEmpty()){
-                        if(dollStack.peek() == board[j][col]){
-                            answer += 2;
-                            dollStack.pop();
-                            board[j][col] = 0;
-                        }else{
-                            dollStack.push(board[j][col]);
-                            board[j][col] = 0;
-                        }
-                    }else{
-                        dollStack.push(board[j][col]);
-                        board[j][col] = 0;
-                    }
-                    break;
+                    realBoard[j+1].add(board[i][j]);
                 }
             }
+        }
+        // 답 변수
+        int count = 0;
+        
+        // moves 돌기
+        Stack<Integer> st = new Stack<Integer>();
+        
+        for(int i = 0; i<moves.length; i++){
+            int pick = -1;
+            // 뽑아서 스택에 넣기
+            if(!realBoard[moves[i]].isEmpty()){
+                pick = (int)realBoard[moves[i]].get(0);
+                realBoard[moves[i]].remove(0);
+                st.add(pick);
+            }else{
+                continue;
+            }
+            // 스택 확인
+            if(st.size() >= 2){
+                int num1 = st.pop();
+                int num2 = st.pop();
+                if(num1 == num2){
+                    count += 2;
+                }else{
+                    st.add(num2);
+                    st.add(num1);
+                }
+                
+            }
+            
             
         }
         
-        return answer;
+
+      
+        
+        return count;
     }
 }
