213877aea96ba082f3682c89e5ae64b8f0e14030
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-23 10:27:17 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.37 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,60 +1,86 @@
+// 맨 처음 왼손 엄지손가락은 * 키패드에 오른손 엄지손가락은 # 키패드 위치
+// 상하좌우 4가지 방향으로만 이동
+// 1, 4, 7을 입력할 때는 왼손
+// 3, 6, 9를 입력할 때는 오른손
+// 2, 5, 8, 0을 입력할 때는 두 엄지손가락의 현재 키패드의 위치에서 더 가까운 엄지손가락
+    //거리가 같다면, 오른손잡이는 오른손 엄지손가락, 왼손잡이는 왼손 엄지손가락
+
+// 왼손의 위치와 오른손의 위치를 계속 저장해 놔야 한다
+// 누르고 나서 누른 손을 답에 계속 저장해야 한다
+// 2 5 8 0 일 경우 왼손, 오른손과의 거리를 계산해야 한다
+    // 
+import java.util.*;
 class Solution {
     public String solution(int[] numbers, String hand) {
-        // numbers를 돌면서 숫자에 맞는 손 저장
-            // 1 4 7 이면 왼손 저장, 3 6 9 면 오른손 저장, 다른 숫자는 거리 계산 후 저장 
-                // 맨해튼 거리: 각 좌표의 x 차이 절댓값 + y 차이 절댓값
-                // 각 숫자별로 좌표를 입력해서 거리 구하기
-            // 위의 과정을 거치면서 왼손 위치와 오른손 위치도 저장해야 함
-        StringBuilder answer = new StringBuilder();
-        int[][] coord = {
-            {3,1},
-            {0,0},
-            {0,1},
-            {0,2},
-            {1,0},
-            {1,1},
-            {1,2},
-            {2,0},
-            {2,1},
-            {2,2},
-            {3,0}, // 10이 들어오면 *
-            {3,2} // 11이 들어오면 #
-        };
-        int leftPos = 10;
-        int rightPos = 11;
+        // 정답
+        StringBuilder sb = new StringBuilder();
         
+        // 왼손 오른손 저장
+        int left = 10;
+        int right = 11;
+        
+        // 2 5 8 0 에서 다른 곳까지의 거리 저장
+        HashMap<Integer, int[]> distance = new HashMap<Integer, int[]>();
+        int[] twoDistance = {3,1,0,1,2,1,2,3,2,3,4,4};
+        int[] fiveDistance = {2,2,1,2,1,0,1,2,1,2,3,3};
+        int[] eightDistance = {1,3,2,3,2,1,2,1,0,1,2,2};
+        int[] zeroDistance = {0,4,3,4,3,2,3,2,1,2,1,1};
+        distance.put(2, twoDistance);
+        distance.put(5, fiveDistance);
+        distance.put(8, eightDistance);
+        distance.put(0, zeroDistance);
+        
+        
+        // numbers를 돌면서 누르는 손 저장
         for(int i = 0; i<numbers.length; i++){
             switch(numbers[i]){
-                case 1, 4, 7:
-                    answer.append("L");
-                    leftPos = numbers[i];
+                case 1:
+                    sb.append("L");
+                    left = 1;
                     break;
-                case 3, 6, 9:
-                    answer.append("R");
-                    rightPos = numbers[i];
+                case 4:
+                    sb.append("L");
+                    left = 4;
+                    break;
+                case 7:
+                    sb.append("L");
+                    left = 7;
+                    break;
+                case 3:
+                    sb.append("R");
+                    right = 3;
+                    break;
+                case 6:
+                    sb.append("R");
+                    right = 6;
+                    break;
+                case 9:
+                    sb.append("R");
+                    right = 9;
                     break;
                 default:
-                    int leftDis = Math.abs(coord[leftPos][0]-coord[numbers[i]][0]) + Math.abs(coord[leftPos][1] - coord[numbers[i]][1]);
-                    int rightDis = Math.abs(coord[rightPos][0]-coord[numbers[i]][0]) + Math.abs(coord[rightPos][1] - coord[numbers[i]][1]);
-                    if(leftDis == rightDis){
-                        if(hand.equals("left")){
-                            leftPos = numbers[i];
-                            answer.append("L");
+                    
+                    int leftDistance = distance.get(numbers[i])[left];
+                    int rightDistance = distance.get(numbers[i])[right];
+                   
+                    if(leftDistance < rightDistance){
+                        sb.append("L");
+                        left = numbers[i];
+                    }else if(leftDistance == rightDistance){
+                        if(hand.equals("right")){
+                            sb.append("R");
+                            right = numbers[i];
                         }else{
-                            rightPos = numbers[i];
-                            answer.append("R");
+                            sb.append("L");
+                            left = numbers[i];
                         }
-                    }else if(leftDis < rightDis){
-                        leftPos = numbers[i];
-                        answer.append("L");
                     }else{
-                        rightPos = numbers[i];
-                        answer.append("R");
+                        sb.append("R");
+                        right = numbers[i];
                     }
-                    break;
             }
         }
         
-        return answer.toString();
+        return sb.toString();
     }
 }
