39245e45796b0b3859887aca5b76a05781380b89
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-24 10:27:33 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.31 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -6,23 +6,9 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put("zero", "0");
-        map.put("one", "1");
-        map.put("two", "2");
-        map.put("three", "3");
-        map.put("four", "4");
-        map.put("five", "5");
-        map.put("six", "6");
-        map.put("seven", "7");
-        map.put("eight", "8");
-        map.put("nine", "9");
         
-        for(String num : numbers){
-            if(s.contains(num)){
-                String newStr = map.get(num);
-                s = s.replace(num, newStr);
-            }
+        for(int i = 0; i<numbers.length; i++){
+            s = s.replaceAll(numbers[i], String.valueOf(i));
         }
      
         return Integer.parseInt(s);
