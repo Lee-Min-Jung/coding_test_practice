330bfbf258a9ca4c7bf511e5c0e6e96f8306864e
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-24 10:19:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,16 +1,30 @@
+// s를 보면서 영어를 포함하고 있는지 확인한 후 포함하고 있으면 해당 영어를 숫자로 바꾼다
+    // zero.....ten 까지 배열을 만들어 놓은 후 해당 배열을 돌면서 contains하고 있는지 확인해서 바꾸기
+    // 바꿀 때는 map에 저장해 놓은 거 찾아와서 바꾸기
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        String[] eng = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        int answer = 0;
+        String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("zero", "0");
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
         
-        for(int i = 0; i<eng.length; i++){
-            if(s.contains(eng[i])){
-                s = s.replaceAll(eng[i], String.valueOf(i));
+        for(String num : numbers){
+            if(s.contains(num)){
+                String newStr = map.get(num);
+                s = s.replace(num, newStr);
             }
         }
-      
-        answer = Integer.parseInt(s);
-        
-        return answer;
+     
+        return Integer.parseInt(s);
     }
 }
