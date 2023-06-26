523c57ae1ba71ecf675af0769c27df23e5401976
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-26 14:02:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.57 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,33 @@
+// participant에는 있는데 completion에는 없는 사람들 구하기
+// participant를 돌기
+    // map에 이름: 1,2... 이런식으로 저장하기
+// completion을 돌기
+    // map에서 이름을 키로 해서 값을 찾고 값에 -1 하기
+// map을 돌기
+    // 각 키의 값이 1인 것이 답
+
 import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> participantMap = new HashMap<String, Integer>();
         
+        // 참여자 돌면서 map 만들기
+        for(String name : participant){
+            participantMap.put(name, participantMap.getOrDefault(name, 0)+1);
+        }
+        
+        // 완주자 돌기
+        for(String name : completion){
+            participantMap.put(name, participantMap.get(name)-1);
+        }
+        
+        // map 돌기
         String answer = "";
-        HashMap<String, Integer> hm = new HashMap<String, Integer>();
-        
-        for(String player : participant){
-            hm.put(player, hm.getOrDefault(player, 0) + 1);
-        }
-        
-        for(String player : completion){
-            hm.put(player, hm.get(player)-1);
-        }
-        
-        for(String key : hm.keySet()){
-            if(hm.get(key) != 0){
+        for(String key : participantMap.keySet()){
+            if(participantMap.get(key) == 1){
                 answer = key;
+                break;
             }
         }
         
