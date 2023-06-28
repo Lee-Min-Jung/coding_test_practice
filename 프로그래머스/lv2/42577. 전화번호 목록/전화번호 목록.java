83199b41a074d461142f3a482a9577662f605371
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-06-28 17:22:02 +0900

    [level 2] Title: 전화번호 목록, Time: 437.28 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,35 +1,39 @@
-// // phone_book 하나씩 돌면서 map으로 저장
-// // phone_book 하나씩 돌면서 각 문자열 살피기
-//     // 문자열을 하나씩 늘려가면서 해당 문자열이 map에 있는지 확인
-//     // 이때 자기 자신인 경우는 제외해야 함
+// phone_book을 글자길이 짧은순으로 정렬 함
+// phone_book을 돌기
+    // map이 비어있으면 map에 전화번호 저장
+    // map이 안 비어 있으면 해당 전화번호 인덱싱하면서 map에 있는지 확인
+    // map에 있으면 false리턴 다 돌고 없으면 true 리턴
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        // 정렬
+        Arrays.sort(phone_book, new Comparator<String>(){
+            public int compare(String str1, String str2){
+                if(str1.length() == str2.length()){
+                    return 0;
+                }
+                return str1.length() - str2.length();
+            }
+        });
+        
     
-        // 저장할 맵
         HashMap<String, Integer> hm = new HashMap<String, Integer>();
         
-        // phone_book 을 map에 저장
+        // phone_book 돌기
         for(int i = 0; i<phone_book.length; i++){
-            hm.put(phone_book[i], 0);
-        }
-        
-        // phone_book 돌면서 문자열 살피기
-        for(int i = 0; i<phone_book.length; i++){
-            for(int j = 1; j<phone_book[i].length(); j++){
-                String prefix = phone_book[i].substring(0, j);
-                // 문자열 돌리다 접두사 발견한 경우
-                if(hm.containsKey(prefix)){
-                    return false;
+            if(hm.isEmpty()){
+                hm.put(phone_book[i], 1);
+            }else{
+                for(int j = 0; j<phone_book[i].length(); j++){
+                    if(hm.getOrDefault(phone_book[i].substring(0, j), 0) == 1){
+                        return false;
+                    }
                 }
+                hm.put(phone_book[i], 1);
             }
         }
         
-        
         return true;
-    
     }    
 }
-
-
