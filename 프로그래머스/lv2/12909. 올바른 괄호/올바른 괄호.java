bd79980e9923ef9734043a7273042ee2e214aa8f
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-07-06 18:00:29 +0900

    [level 2] Title: 올바른 괄호, Time: 18.86 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,38 @@
+// s를 돈다
+    // 여는 괄호면 스택에 넣는다
+    // 닫는 괄호면 스택에서 pop 하면서 여는 괄호인지 확인한다
+        // 만약 여는 괄호 아니면 바로 올바르지 않은 거니까 멈추기
+    // 다 하고 나서 스택 비어있지 않으면 올바르지 않은 것
+
+
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<Character>();
+        
+        // s 돌기
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            // 여는 괄호면 스택에 넣기
+            if(ch == '('){
+                st.push(ch);
+                continue;
+            }
+            // 닫는 괄호면 스택에서 pop하면서 여는 괄호 나오는지 확인
+            if(ch == ')'){
+                if(!st.isEmpty() && st.pop() == '('){
+                    continue;
+                }else{
+                    return false;
+                }
+            }
+            
+        }
+        // 마지막에 스택 확인
+        if(!st.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+}
