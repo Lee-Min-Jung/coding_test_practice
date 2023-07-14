7d1a81838893f847b5ad06c2e7ffc9fc7c8be2cd
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-07-14 18:24:00 +0900

    [level 2] Title: 소수 찾기, Time: 2.51 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,58 +1,48 @@
-// 문자열을 분해해서 하나씩 담기
-// 해당 문자열로 만들 수 있는 모든 숫자 경우의 수 구하기
-    // dfs 돌면서 dfs에 딱 들어왔을 때 소수인지 확인을 한다
-// 위에서 구한 모든 경우의 수가 소수인지 확인하기
 import java.util.*;
 
+
 class Solution {
-    static boolean[] visited;
-    static int answer;
-    static String[] str;
+    static boolean[] visited = new boolean[7];
     static HashSet<Integer> numSet = new HashSet<Integer>();
+    static String[] arr;
 
     
     public int solution(String numbers) {
-        // 세팅
-        visited = new boolean[numbers.length()];
-        answer = 0;
+        arr = numbers.split("");
         
-        // 문자열 분해
-        str = numbers.split("");
-        
-        // 모든 경우의 수 
-        for(int i = 0; i<str.length; i++){
-            dfs(i, "", numSet);
+        // arr 돌기
+        for(int i = 0; i<arr.length; i++){
+            recur("", i);
         }
         
-        return answer;
-        
-    }
-    static void dfs(int nodeIndex, String beforeStr, HashSet<Integer> numSet){
-        visited[nodeIndex] = true;
-        String numStr = beforeStr + str[nodeIndex];
-        int num = Integer.parseInt(numStr);
-        
-        if(!numSet.contains(num)){
-            numSet.add(num);
-            if(isSosu(num)){
-                System.out.println(num);
-                answer++;
-            }
-        }
-        
-        
-        
-        for(int i = 0; i<str.length; i++){
-            if(!visited[i]){
-                dfs(i, numStr, numSet);
-            }
-        }
-        
-        visited[nodeIndex] = false;
+        // 정답 리턴
+        return numSet.size();
     }
     
-    static boolean isSosu(int num){
-        if(num == 1 | num == 0){
+    // 완전탐색 할 함수
+    public void recur(String str, int index){
+        visited[index] = true;
+        String targetStr = str+arr[index];
+        int targetInt = Integer.parseInt(targetStr);
+        
+        // set에 있는지 확인 및 소수 확인
+        if(!numSet.contains(targetInt) && checkSosu(targetInt)){
+            numSet.add(targetInt);
+        }
+        
+        for(int i = 0; i<arr.length; i++){
+            if(!visited[i]){
+                recur(targetStr, i);
+            }
+        }
+        
+        visited[index] = false;
+        
+    }
+    
+    // 소수 확인 함수
+    public boolean checkSosu(int num){
+        if(num == 0 || num == 1){
             return false;
         }
         for(int i = 2; i<num; i++){
