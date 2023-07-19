cfa170d948094253fc301eb63705f111a31ee3fe
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-07-19 10:30:13 +0900

    [level 2] Title: 타겟 넘버, Time: 0.31 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,40 +1,40 @@
-// numbers의 맨 첫수의 양수를 dfs로 돌린다
-//                    음수를 dfs로 돌린다
-// 각 dfs 수행하며 다음 원소로 갈 때 플러스 마이너스 값을 다 살펴야 한다.
-import java.util.*;
+// 생각
+    // numbers를 돌면서 가지치기 하면서 양수 음수로 나누어 numbers 끝까지 경우를 구한 후 수 다 더하면 되는데..
+    // 깊이 탐색하면 되니까 dfs인듯
+// 구현
+    // numbers의 맨 첫 원소의 양수를 시작으로 해서 dfs를 돈다
+    // numbers의 맨 첫 원소의 음수를 시작으로 해서 dfs를 돈다
+    // dfs
+        // 들어온 원소의 인덱스, 지금까지의 합, numbers 배열을 매개변수로 받는다
+        // 들어온 인덱스 다음부터 다시 dfs를 돈다
+            // 이때 음수 양수로 나누어 dfs를 돌게 해야 한다
+        // 들어온 인덱스가 numbers의 길이와 같으면
+            // 지금까지의 합이 target과 같은지 비교한 후 답을 1 증가
 
 class Solution {
-    static int[] numbers;
+    static int answer = 0;
     static int target;
     
     public int solution(int[] numbers, int target) {
-        // 세팅
-        this.numbers = numbers;
         this.target = target;
         
-        // 탐색 수행
+        // dfs 돌기
+        dfs(0, 0, numbers);
         
-        return dfs(0,0);
-        
-        
-        
+        // 답 리턴
+        return answer;
+
     }
     
-    static int dfs(int nodeIndex, int sum){
-        int answer = 0;
-        
-        if(nodeIndex == numbers.length){
+    public void dfs(int index, int sum, int[] numbers){
+        if(index == numbers.length){
             if(sum == target){
-                return 1;
-            }else{
-                return 0;
+                answer += 1;
             }
+        }else{
+            dfs(index+1, sum+numbers[index], numbers);
+            dfs(index+1, sum-numbers[index], numbers);
         }
-        answer += dfs(nodeIndex+1, sum+numbers[nodeIndex]);
-        answer += dfs(nodeIndex+1, sum-numbers[nodeIndex]);
-        
-        return answer;
+       
     }
 }
-
-
