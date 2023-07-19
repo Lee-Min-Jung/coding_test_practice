7de757563fc9313ed97482b00f879ad8a7554c67
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-07-19 12:00:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.10 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,62 +1,61 @@
+// 0: 벽, 1: 벽 아님
+// 생각   
+    // 시작점부터 시작해서 상하좌우를 살피면서 1이면 거기로 가고 0이면 안 가고
+    // 근데 최단 경로로 가야 하니까 bfs
+// 구현
+    // 시작점을 bfs에 넣기
+    // bfs
+        // 시작점과 maps를 매개변수로 받기
+        // bfs 위한 큐 생성
+        // 시작점을 큐에 넣기
+        // 큐가 비어있지 않을 때까지 반복
+            // 큐에서 하나 빼고 방문했다고 표시하고 길이 1 증가
+            // 큐에서 뺀 거 기준으로 상하좌우 돌기 위해 for문으로 4번 돌기
+                // 시작점 기준 상하좌우가 maps 안에 있으면서 1이면 해당 위치를 큐에 넣기
 import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        // 시작점부터 각 위치까지의 거리 저장 배열
-        int[][] distance = new int[maps.length][maps[0].length];
+        return bfs(0, 0, maps);
+    }
+    
+    public int bfs(int x, int y, int[][] maps) {
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {1, -1, 0, 0};
         
-        // 거리 초기화
-        for(int i = 0; i<maps.length; i++){
-            for(int j = 0; j<maps[i].length; j++){
-                distance[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        distance[0][0] = 1;
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
         
-        // 동서남북 조회 위한 배열
-        int[] dx = {1,-1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        
-        // bfs 돌기
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0,0});
+        int[] startPos = {x, y};
+        q.add(startPos);
+        visited[x][y] = true;
         
-        while(!q.isEmpty()){
-            int[] curr = q.poll();
-            int x = curr[0];
-            int y = curr[1];
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int curX = cur[0];
+            int curY = cur[1];
             
-            // 현재 위치에서 동서남북 확인
-            for(int i = 0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            if (curX == maps.length - 1 && curY == maps[0].length - 1) {
+                return maps[curX][curY];
+            }
+            
+            for (int i = 0; i < 4; i++) {
+                int nextX = curX + dx[i];
+                int nextY = curY + dy[i];
                 
-                // maps 벗어나는 경우
-                if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length){
-                    continue;
-                }
-                
-                // 벽 만나는 경우
-                if(maps[nx][ny] == 0){
-                    continue;
-                }
-                
-                // 다음 위치까지의 거리가 현재 위치까지의 거리 + 1 보다 큰 경우에만 추가 탐색 진행
-                if(distance[nx][ny] > distance[x][y] + 1){
-                    distance[nx][ny] = distance[x][y] + 1;
-                    q.offer(new int[]{nx, ny});
+                if (nextX >= 0 && nextX < maps.length && nextY >= 0 && nextY < maps[0].length) {
+                    if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
+                        maps[nextX][nextY] = maps[curX][curY] + 1;
+                        visited[nextX][nextY] = true;
+                        int[] nextPos = {nextX, nextY};
+                        q.add(nextPos);
+                    }
                 }
             }
-        
-        }
-        // 도착점까지의 거리를 찾아서 저장
-        int answer = distance[maps.length - 1][maps[0].length - 1];
-        
-        if(answer == Integer.MAX_VALUE){
-            return -1;
         }
         
-        return answer;
-        
+        return -1;
     }
 }
+
+
