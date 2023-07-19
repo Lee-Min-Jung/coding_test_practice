c64d84013cfb9d3400aa0608396ad8175b0e237a
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-07-19 12:21:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.43 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,61 +1,50 @@
-// 0: 벽, 1: 벽 아님
-// 생각   
-    // 시작점부터 시작해서 상하좌우를 살피면서 1이면 거기로 가고 0이면 안 가고
-    // 근데 최단 경로로 가야 하니까 bfs
-// 구현
-    // 시작점을 bfs에 넣기
-    // bfs
-        // 시작점과 maps를 매개변수로 받기
-        // bfs 위한 큐 생성
-        // 시작점을 큐에 넣기
-        // 큐가 비어있지 않을 때까지 반복
-            // 큐에서 하나 빼고 방문했다고 표시하고 길이 1 증가
-            // 큐에서 뺀 거 기준으로 상하좌우 돌기 위해 for문으로 4번 돌기
-                // 시작점 기준 상하좌우가 maps 안에 있으면서 1이면 해당 위치를 큐에 넣기
 import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
         return bfs(0, 0, maps);
     }
-    
+
     public int bfs(int x, int y, int[][] maps) {
         int[] dx = {0, 0, -1, 1};
         int[] dy = {1, -1, 0, 0};
-        
+
         boolean[][] visited = new boolean[maps.length][maps[0].length];
-        
+
         Queue<int[]> q = new LinkedList<>();
         int[] startPos = {x, y};
         q.add(startPos);
         visited[x][y] = true;
-        
+
+        int answer = 1; // 거리를 기록하는 변수
+
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curX = cur[0];
-            int curY = cur[1];
-            
-            if (curX == maps.length - 1 && curY == maps[0].length - 1) {
-                return maps[curX][curY];
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                int nextX = curX + dx[i];
-                int nextY = curY + dy[i];
-                
-                if (nextX >= 0 && nextX < maps.length && nextY >= 0 && nextY < maps[0].length) {
-                    if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
-                        maps[nextX][nextY] = maps[curX][curY] + 1;
-                        visited[nextX][nextY] = true;
-                        int[] nextPos = {nextX, nextY};
-                        q.add(nextPos);
+            int size = q.size();
+            for (int k = 0; k < size; k++) {
+                int[] cur = q.poll();
+                int curX = cur[0];
+                int curY = cur[1];
+
+                if (curX == maps.length - 1 && curY == maps[0].length - 1) {
+                    return answer; // 도착지에 도달한 경우 거리 반환
+                }
+
+                for (int i = 0; i < 4; i++) {
+                    int nextX = curX + dx[i];
+                    int nextY = curY + dy[i];
+
+                    if (nextX >= 0 && nextX < maps.length && nextY >= 0 && nextY < maps[0].length) {
+                        if (!visited[nextX][nextY] && maps[nextX][nextY] == 1) {
+                            visited[nextX][nextY] = true;
+                            int[] nextPos = {nextX, nextY};
+                            q.add(nextPos);
+                        }
                     }
                 }
             }
+            answer++; // 한 번의 BFS 탐색이 끝날 때마다 거리를 1씩 증가
         }
-        
+
         return -1;
     }
 }
-
-
