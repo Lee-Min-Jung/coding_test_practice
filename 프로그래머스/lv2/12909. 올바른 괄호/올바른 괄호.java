6eddebec63f4361d9902ba637aec7aaf5dbd7ed2
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-07-20 11:50:16 +0900

    [level 2] Title: 올바른 괄호, Time: 20.97 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,39 +1,39 @@
-// s를 돈다
-    // 여는 괄호면 스택에 넣는다
-    // 닫는 괄호면 스택에서 pop 하면서 여는 괄호인지 확인한다
-        // 만약 여는 괄호 아니면 바로 올바르지 않은 거니까 멈추기
-    // 다 하고 나서 스택 비어있지 않으면 올바르지 않은 것
-
-
+// 생각
+    // 괄호 검사는 너무나 유명한 스택문제인데
+// 구현
+    // s를 돈다
+        // 여는 괄호면 스택에 넣는다
+        // 닫는 괄호면 스택에서 하나 빼서 여는 괄호인지 확인한다
+            // 만약 비어 있으면 false
+            // 여는 괄호 아니면 멈추고 false 리턴
+    // 다 돌았는데 스택에 뭐 있으면 false
 import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> st = new Stack<Character>();
-        char[] charArr = s.toCharArray();
+    
         
         // s 돌기
-        for(Character ch : charArr){
-        
-            // 여는 괄호면 스택에 넣기
+        Stack<Character> st = new Stack<Character>();
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            
             if(ch == '('){
                 st.push(ch);
-                continue;
-            }
-            // 닫는 괄호면 스택에서 pop하면서 여는 괄호 나오는지 확인
-            if(ch == ')'){
-                if(!st.isEmpty() && st.pop() == '('){
-                    continue;
-                }else{
+            }else if(ch == ')'){
+                if(st.isEmpty()){
+                    return false;
+                }
+                if(st.pop() != '('){
                     return false;
                 }
             }
-            
         }
-        // 마지막에 스택 확인
+        
         if(!st.isEmpty()){
             return false;
         }
+        
         return true;
     }
 }
