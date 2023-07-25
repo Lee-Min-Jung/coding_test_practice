22ddd9cad342b45ec9c4fca86e9de71d2591dfb1
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-07-25 10:24:14 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        
+        while(true){
+            a = a/2 + a%2;
+            b = b/2 + b%2;
+            answer += 1;
+            
+            if(a == b){
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
