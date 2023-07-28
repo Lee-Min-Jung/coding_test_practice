c9a7c1c128a7cf2901ce3ef320fc4cdb2403d458
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-07-28 10:36:29 +0900

    [level 2] Title: H-Index, Time: 0.38 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,23 +1,40 @@
-// 0 1 3 5 6 값
-// 0 1 2 3 4 인덱스
-// citations[i] 는 현재 원소의 인용 횟수
-// citations.length-i 는 현재 원소보다 크거나 같은 인용 횟수 가진 논문의 개수
-// 둘 중 작은 값이 h-index의 후보다. 
-    // 만약 citations[i] = 5, citations.length - i = 3 이라고 할 때
-    // h-index의 정의에 따라 5번 이상 인용된 논문이 5편이상 이어야 5가 h-index될 수 있는데
-    // 그 원소보다 인용 횟수가 크거나 같은 논문 개수가 더 적어서 5는 h-index가 될 수 없다
+// 생각
+    // citations 정렬 후 반복문 돌기
+// 구현
+    // h-index 최댓값은 citations의 길이부터 시작가능
+    // citations 정렬
+    // citations를 돈다(내림차순으로)
+        // 각 값이 h-index보다 크거나 같으면 지금까지 count 1 증가 후 다음으로
+        // 각 값이 h-index보다 작으면 h-index와 count비교해서 다르면 종료 후 h-index 1 줄이고 다시 반복
+        // 각 값이 h-index보다 작으면 h-index와 count비교해서 같으면 해당 h-index 리턴
 
 import java.util.*;
 
-
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
+        int h_index = citations.length;
+        
+        // 정렬
         Arrays.sort(citations);
-        for(int i=0; i<citations.length; i++){
-            int smaller = Math.min(citations[i], citations.length-i);
-            answer = Math.max(answer, smaller);
+        
+        // 반복문 돌기
+        int count = 0;
+        
+        for(int i = citations.length-1; i>=0; i--){
+            int cur = citations[i];
+            if(cur >= h_index){
+                count += 1;
+            }else{
+                if(count < h_index){
+                    h_index -= 1;
+                    count = 0;
+                    i = citations.length;
+                }else{
+                    break;
+                }
+            }
         }
-        return answer;
+        
+        return h_index;
     }
 }
