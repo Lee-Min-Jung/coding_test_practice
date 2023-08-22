dc10c9a90b4464437f81253958ded27efaa3f49d
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-08-22 12:07:17 +0900

    [level 2] Title: 전화번호 목록, Time: 292.44 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,40 +1,25 @@
-// phone_book을 글자길이 짧은순으로 정렬 함
-// phone_book을 돌기
-    // map이 비어있으면 map에 전화번호 저장
-    // map이 안 비어 있으면 해당 전화번호 인덱싱하면서 map에 있는지 확인
-    // map에 있으면 false리턴 다 돌고 없으면 true 리턴
+// 생각
+    // phone_book을 사전순으로 정렬하면 다음 것이 본인으로 시작하는지만 for문 한 번에 확인하면 된다
+// 구현
+    // phone_book 사전 정렬(그냥 정렬하면 됨)
+    // phone_book 돌기
+        // 다음 것이 본인 것으로 시작하면 false 리턴
+    // true 리턴
+
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // 정렬
+        // 사전순 정렬
         Arrays.sort(phone_book);
-        // Arrays.sort(phone_book, new Comparator<String>(){
-        //     public int compare(String str1, String str2){
-        //         if(str1.length() == str2.length()){
-        //             return 0;
-        //         }
-        //         return str1.length() - str2.length();
-        //     }
-        // });
         
-    
-        HashMap<String, Integer> hm = new HashMap<String, Integer>();
-        
-        // phone_book 돌기
-        for(int i = 0; i<phone_book.length; i++){
-            if(hm.isEmpty()){
-                hm.put(phone_book[i], 1);
-            }else{
-                for(int j = 0; j<phone_book[i].length(); j++){
-                    if(hm.getOrDefault(phone_book[i].substring(0, j), 0) == 1){
-                        return false;
-                    }
-                }
-                hm.put(phone_book[i], 1);
+        // for문 돌기
+        for(int i = 0; i<phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
             }
         }
         
         return true;
-    }    
+    }
 }
