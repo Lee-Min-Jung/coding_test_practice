89bec168a00de6739922debda8702cbff5839fa8
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-08-22 10:53:58 +0900

    [level 2] Title: 타겟 넘버, Time: 0.45 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,40 +1,46 @@
 // 생각
-    // numbers를 돌면서 가지치기 하면서 양수 음수로 나누어 numbers 끝까지 경우를 구한 후 수 다 더하면 되는데..
-    // 깊이 탐색하면 되니까 dfs인듯
+    // numbers 각각을 돌 때 플러스 마이너스 각각을 붙여가며 가지치기해서 결과를 확인해야 함  \
+    // numbers를 돌면서 값 하나를 확인하며 재귀적으로 플러스 마이너스 붙인 함수를 부르기
 // 구현
-    // numbers의 맨 첫 원소의 양수를 시작으로 해서 dfs를 돈다
-    // numbers의 맨 첫 원소의 음수를 시작으로 해서 dfs를 돈다
-    // dfs
-        // 들어온 원소의 인덱스, 지금까지의 합, numbers 배열을 매개변수로 받는다
-        // 들어온 인덱스 다음부터 다시 dfs를 돈다
-            // 이때 음수 양수로 나누어 dfs를 돌게 해야 한다
-        // 들어온 인덱스가 numbers의 길이와 같으면
-            // 지금까지의 합이 target과 같은지 비교한 후 답을 1 증가
+    // 재귀함수 호출
+    // 전역 변수로 answer 선언
+    // 재귀 함수
+        // 지금까지의 합, numbers 배열, 현재 인덱스를 매개변수로 받기
+        // 현재 인덱스+1 == numbers의 길이
+            // 지금까지의 합이 target과 같다
+                // 정답 1 증가
+            // 지금까지의 합이 target과 같지 않다
+                // 패스
+        // 현재 인덱스 < numbers의 길이
+            // 지금까지의 합에 현재 들어온 수 더하기
+            // 다음에 살펴야 할 값에 플러스 붙인 재귀 함수 호출
+            // 다음에 살펴야 할 값에 마이너스 붙인 재귀 함수 호출
+
 
 class Solution {
-    static int answer = 0;
-    static int target;
+    static int answer;
+    static int targetNum;
     
     public int solution(int[] numbers, int target) {
-        this.target = target;
+        // 초기화
+        answer = 0;
+        targetNum = target;
         
-        // dfs 돌기
-        dfs(0, 0, numbers);
+        // 재귀함수 호출
+        recur(0, numbers, 0);
         
-        // 답 리턴
+        // 정답
         return answer;
-
     }
     
-    public void dfs(int index, int sum, int[] numbers){
+    public void recur(int sum, int[] numbers, int index){
         if(index == numbers.length){
-            if(sum == target){
-                answer += 1;
+            if(sum == targetNum){
+                answer++;
             }
         }else{
-            dfs(index+1, sum+numbers[index], numbers);
-            dfs(index+1, sum-numbers[index], numbers);
+            recur(sum+numbers[index], numbers, index+1);
+            recur(sum-numbers[index], numbers, index+1);
         }
-       
     }
 }
