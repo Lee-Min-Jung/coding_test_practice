eef55acaca068723df6b6402c9b9e7f52463da76
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-08-29 15:04:05 +0900

    [level 1] Title: 최소직사각형, Time: 6.74 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,32 +1,33 @@
-// 가로와 세로 중 큰 거를 가로로, 작은 거를 세로로
-// 가로 중 가장 큰 것과 세로 중 가장 큰 거 찾아서 곱하기
-
-
+// 생각
+    // 완전탐색
+// 구현
+    // sizes 돌기
+        // 가로 세로 중 더 큰 걸 가로에 놓기
+        // 가로 배열에 가로 추가, 세로 배열에 세로 추가
+    // 가로 세로 배열 정렬
+    // 맨 뒤에 있는 것 끼리 곱하기
+import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
-        // sizes 돌면서 가로 세로 설정
-        for(int i = 0; i<sizes.length; i++){
-            if(sizes[i][0] < sizes[i][1]){
-                int temp = sizes[i][1];
-                sizes[i][1] = sizes[i][0];
-                sizes[i][0] = temp;
-            }
-        }
-        
-        // 가로 세로 중 가장 큰 값 찾기
-        int maxWidth = sizes[0][0];
-        int maxHeight = sizes[0][1];
+        // sizes 돌기
+        int[] widthArr = new int[sizes.length];
+        int[] heightArr = new int[sizes.length];
         
         for(int i = 0; i<sizes.length; i++){
-            if(sizes[i][0] > maxWidth){
-                maxWidth = sizes[i][0];
-            }
-            if(sizes[i][1] > maxHeight){
-                maxHeight = sizes[i][1];
-            }
+            int width = Math.max(sizes[i][0], sizes[i][1]);
+            int height = Math.min(sizes[i][0], sizes[i][1]);
+            widthArr[i] = width;
+            heightArr[i] = height;
         }
-        System.out.println(maxWidth);
-        System.out.println(maxHeight);
-        return maxWidth * maxHeight;
+        
+        // 정렬
+        Arrays.sort(widthArr);
+        Arrays.sort(heightArr);
+        
+        // 답
+        int answer = 0;
+        answer = widthArr[widthArr.length-1] * heightArr[heightArr.length-1];
+        
+        return answer;
     }
 }
