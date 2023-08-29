ac811d347528919cbe5cc2f3a65cdd8e5a73cfb9
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-08-29 17:41:50 +0900

    [level 2] Title: 소수 찾기, Time: 2.38 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -4,39 +4,32 @@ import java.util.*;
 class Solution {
     static boolean[] visited = new boolean[7];
     static HashSet<Integer> numSet = new HashSet<Integer>();
-    static String[] arr;
+
 
     
     public int solution(String numbers) {
-        arr = numbers.split("");
-        
-        // arr 돌기
-        for(int i = 0; i<arr.length; i++){
-            recur("", i);
-        }
+        String[] arr = numbers.split("");
+        recur("", 0, arr);
+
         
         // 정답 리턴
         return numSet.size();
     }
     
     // 완전탐색 할 함수
-    public void recur(String str, int index){
-        visited[index] = true;
-        String targetStr = str+arr[index];
-        int targetInt = Integer.parseInt(targetStr);
-        
-        // set에 있는지 확인 및 소수 확인
-        if(!numSet.contains(targetInt) && checkSosu(targetInt)){
-            numSet.add(targetInt);
-        }
-        
+    public void recur(String str, int index, String[] arr){
         for(int i = 0; i<arr.length; i++){
             if(!visited[i]){
-                recur(targetStr, i);
+                visited[i] = true;
+                String targetStr = str+arr[i];
+                int targetInt = Integer.parseInt(targetStr);
+                if(checkSosu(targetInt)){
+                    numSet.add(targetInt);
+                }
+                recur(targetStr, index+1, arr);
+                visited[i] = false;
             }
         }
-        
-        visited[index] = false;
         
     }
     
