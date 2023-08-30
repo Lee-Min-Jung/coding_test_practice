463cf96c6127a223eceb79f677da4f06b0b9c0c7
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-08-30 14:29:55 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,31 +1,34 @@
-// 가로가 더 길거나 같다
 // 생각
-    // yellow를 어떻게 배치하냐에 따라 brown의 개수가 달라진다
-    // 주어진 yellow를 배치할 수 있는 각 방법에 따라 brown 개수를 구한 후 주어진 거라 비교해본다
+    // yellow의 개수에 따라 모양이 빠귀고 가로 세로가 결정된다
 // 구현
-    // yellow를 돌면서 yellow의 약수를 구한다
-    // 약수와 약수로 나눈 수 중 큰 수를 가로로 놓고, 작은 수를 세로로 놓는다
-    // 해당 가로와 세로에 따른 brown의 개수를 구한다
-        // 가로*2 + 세로 *2 + 4
-    // 위에서 구한 거와 주어진 brown을 비교해서 같으면 
-        // 가로 = 위에서 구한 yellow 가로 + 2, 세로 = 위에서 구한 yellow 세로 + 2
+    // while을 돈다
+        // if(yellow % num == 0)
+            // yellow width = num, yellow height = yellow / num
+            // brown width = yellow width + 2, brown height = yellow height + 2
+            // brown count = brown width * brown height - yellow width * yellow height
+            // if(brown count == brown)
+                // brown width와 brown height 리턴
+        // num 1 증가
 
 class Solution {
     public int[] solution(int brown, int yellow) {
+        int num = 1;
         int[] answer = new int[2];
         
-        // yellow 돌기
-        for(int i = 1; i<=yellow; i++){
-            if(yellow % i == 0){
-                int width = yellow / i;
-                int height = i;
-                int tempBrown = width * 2 + height * 2 + 4;
-                if(tempBrown == brown){
-                    answer[0] = width + 2;
-                    answer[1] = height + 2;
-                    break;
+        // while로 yellow 약수 돌기
+        while(num <= yellow){
+            if(yellow % num == 0){
+                int yellowWidth = num;
+                int yellowHeight = yellow / num;
+                int brownWidth = yellowWidth + 2;
+                int brownHeight = yellowHeight + 2;
+                int brownCount = brownWidth * brownHeight - yellowWidth * yellowHeight;
+                if(brownCount == brown){
+                    answer[0] = brownWidth;
+                    answer[1] = brownHeight;
                 }
             }
+            num++;
         }
         
         return answer;
