f9b4a0f428249bc42feb2bffd90fe1578125aae4
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-08-30 16:16:25 +0900

    [level 2] Title: 모음 사전, Time: 10.73 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,56 +1,41 @@
 // 생각
-    // 감이 안 잡힌다... 그래서 다른 걸 참고 함
+    // 만들 수 있는 단어들을 만들다가 word랑 같은 단어 나오면 인덱스 리턴
 // 구현
-    // dfs 돌면서 만든 단어들을 담을 list를 만든다
-    // words를 돌면서 각 words를 시작점으로 해서 dfs를 돈다
-    // 만든 단어 담긴 list를 돌면서 word와 같은 것의 index를 리턴한다
-    
-    // dfs
-        // 그 동안 만들어진 단어들 담을 list와 앞으로 담을 단어를 매개변수로 보낸다
-        // 앞으로 만들 단어가 만들 수 있는 최대 단어 길이보다 크면 함수 종료
-        // 단어 담을 list에 앞으로 담을 단어 없으면 넣기
-        // for문으로 words를 돌기
-            // 재귀적으로 dfs 호출
-
+    // 알파벳 담은 배열 선언
+    // for(알파벳 배열) : 재귀 함수에 각 알파벳 보내기
+    // 재귀 함수(String str, int len, 알파벳 배열)
+        // if(len > 5) : 종료
+        // count++
+        // if(str == word) : count 리턴
+        // for(알파벳 배열) : 재귀 함수에 각 알파벳 보내기
 import java.util.*;
 class Solution {
-    static char[] ch = {'A', 'E', 'I', 'O', 'U'};
-    static int maxLen = 5;
-    
+    static ArrayList<String> wordList;
     public int solution(String word) {
-
+        // 초기화
+        wordList = new ArrayList<String>();
         int answer = 0;
         
-        // 만들어진 단어 담기
-        ArrayList<String> wordList = new ArrayList<String>();
+        // 알파벳 배열
+        String[] strArr = {"A", "E", "I", "O", "U"};
         
-        // ch돌기
-        for(int i = 0; i<ch.length; i++){
-            dfs(wordList, String.valueOf(ch[i]));
+        // 알파벳 배열 돌기
+        for(int i = 0; i<strArr.length; i++){
+            recur(strArr[i], strArr);
         }
-        
-        // 몇번째인지 찾기
-        for(int i = 0; i<wordList.size(); i++){
-            if(wordList.get(i).equals(word)){
-                answer = i+1;
-                break;
-            }
-        }
-        
+    
+        // index 찾기
+        answer = wordList.indexOf(word)+1;
         return answer;
-        
-        
     }
-    public void dfs(ArrayList<String> wordList, String str){
-        if(str.length() > maxLen){
+    
+    public void recur(String str, String[] strArr){
+        if(str.length() > 5){
             return;
         }
-        if(!wordList.contains(str)){
-            wordList.add(str);
-        }
-        for(int i = 0; i<ch.length; i++){
-            dfs(wordList, str+String.valueOf(ch[i]));
+        wordList.add(str);
+        for(int i = 0; i<strArr.length; i++){
+            recur(str+strArr[i], strArr);
         }
     }
 }
-
