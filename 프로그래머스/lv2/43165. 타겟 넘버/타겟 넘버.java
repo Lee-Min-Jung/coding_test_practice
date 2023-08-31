be0d9bf5c4ec16d3ffed57a92a6c03c900a24264
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-08-31 11:34:43 +0900

    [level 2] Title: 타겟 넘버, Time: 1.51 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,46 +1,35 @@
 // 생각
-    // numbers 각각을 돌 때 플러스 마이너스 각각을 붙여가며 가지치기해서 결과를 확인해야 함  \
-    // numbers를 돌면서 값 하나를 확인하며 재귀적으로 플러스 마이너스 붙인 함수를 부르기
+    // 재귀로 계속 하다가 종료조건 오면 종료해야할듯
 // 구현
-    // 재귀함수 호출
-    // 전역 변수로 answer 선언
-    // 재귀 함수
-        // 지금까지의 합, numbers 배열, 현재 인덱스를 매개변수로 받기
-        // 현재 인덱스+1 == numbers의 길이
-            // 지금까지의 합이 target과 같다
-                // 정답 1 증가
-            // 지금까지의 합이 target과 같지 않다
-                // 패스
-        // 현재 인덱스 < numbers의 길이
-            // 지금까지의 합에 현재 들어온 수 더하기
-            // 다음에 살펴야 할 값에 플러스 붙인 재귀 함수 호출
-            // 다음에 살펴야 할 값에 마이너스 붙인 재귀 함수 호출
-
+    // 재귀(int[] numbers, int index, int sum)
+        // if(index == numbers.length-1)
+            // if(sum == target) : count++
+            // 종료
+        // 재귀(numbers, index+1, sum+현재값)
+        // 재귀(numbers, index+1, sum-현재값)
 
 class Solution {
+    static int targetCopy;
     static int answer;
-    static int targetNum;
-    
     public int solution(int[] numbers, int target) {
         // 초기화
+        targetCopy = target;
         answer = 0;
-        targetNum = target;
-        
-        // 재귀함수 호출
-        recur(0, numbers, 0);
+        // 재귀에 넣기
+        recur(numbers, 0, 0);
         
         // 정답
         return answer;
     }
     
-    public void recur(int sum, int[] numbers, int index){
+    public void recur(int[] numbers, int index, int sum){
         if(index == numbers.length){
-            if(sum == targetNum){
+            if(sum == targetCopy){
                 answer++;
             }
-        }else{
-            recur(sum+numbers[index], numbers, index+1);
-            recur(sum-numbers[index], numbers, index+1);
+            return;
         }
+        recur(numbers, index+1, sum+numbers[index]);
+        recur(numbers, index+1, sum-numbers[index]);
     }
 }
