1a7a9ef91ddbc4505728fe9f12db8eba6a7cb4a9
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-09-06 11:43:52 +0900

    [level 2] Title: 소수 찾기, Time: 3.90 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,47 +1,51 @@
+// 생각
+    // numbers를 쪼갠 후 만들 수 있는 모든 경우의 수 만들어 보면서 소수인 경우 세기
+    // 재귀 dfs
+// 구현
+    // numbers를 쪼개서 배열로 만든 후 맨 첫 원소부터 넣어서 재귀 돌리기
+    // dfs(String arr, )
+    // checkSosu(int num)
 import java.util.*;
-
 class Solution {
     static boolean[] visited;
-    static HashSet<Integer> hs = new HashSet<Integer>();
+    static int answer = 0;
+    static HashSet<Integer> set = new HashSet<Integer>();
     
     public int solution(String numbers) {
-        
-        // 변수 초기화
         String[] arr = numbers.split("");
         visited = new boolean[arr.length];
         
-        // 재귀 시작
-        recur(0, arr, "");
+        // 재귀
+        recur(arr, "");
         
-        return hs.size();
+        return set.size();
     }
     
-    public void recur(int index, String[] arr, String str){
+    public void recur(String[] arr, String numStr){
+        if(numStr.length() == arr.length){
+            return;
+        }
         for(int i = 0; i<arr.length; i++){
             if(!visited[i]){
-                visited[i] = true;
-                String targetStr = str + arr[i];
-                int targetInt = Integer.parseInt(targetStr);
-                if(isSosu(targetInt)){
-                    hs.add(targetInt);
+                if(checkSosu(Integer.parseInt(numStr+arr[i]))){
+                    set.add(Integer.parseInt(numStr+arr[i]));
                 }
-                recur(index+1, arr, targetStr);
+                visited[i] = true;
+                recur(arr, numStr+arr[i]);
                 visited[i] = false;
             }
         }
     }
     
-    public boolean isSosu(int num){
-        if(num == 1 || num == 0){
+    public boolean checkSosu(int num){
+        if(num == 0 || num == 1){
             return false;
         }
-        
         for(int i = 2; i<=Math.sqrt(num); i++){
             if(num % i == 0){
                 return false;
             }
         }
-        
         return true;
     }
 }
