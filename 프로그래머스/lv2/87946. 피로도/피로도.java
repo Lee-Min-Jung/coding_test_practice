6d91c6b3f23c016a3d6b3fb6583c99bc118191af
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-09-06 14:01:17 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,49 +1,31 @@
 // 생각
-    // 던전을 돌 수 있는 모든 경우의 수를 돌면서 총 몇 개를 방문할 수 있는지 세어야 함
-    // 완전탐색, 재귀로 구현 가능
+    // dungeons를 차례로 돌면서 모두 탐험 가능한지 세기
+    // 재귀 활용한 dfs
 // 구현
-    // 방문배열 초기화
-    // 재귀 함수에 매개변수로 주어야 할 것들
-        // 던전 배열, 현재 피로도, 현재까지 방문한 개수 
-    // 재귀 함수
-        // 던전 돌기
-            // 현재 던전 방문 안 함 && 현재 피로도 >= 현재 방문한 던전의 최소 필요 피로도
-                // 현재 피로도 -= 사용 피로도
-                // 방문 표시
-                // count 증가
-                // 재귀함수 다시 방문
-                // 방문 취소
-            // 현재 던전 방문 || 현재 피로도 < 현재 방문한 던전의 최소 필요 피로도    
-                // 그냥 패스
-        // for문 돌면서 얻은 count와 현재 answer중 최댓값으로 answer 갱신
+    // recur()
 
 class Solution {
     static boolean[] visited;
-    static int answer;
+    static int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
-        // 초기화
+        // 변수
         visited = new boolean[dungeons.length];
-        answer = 0;
         
-        // 재귀함수에 보내기
-        recur(k, dungeons, 0);
+        // 재귀
+        recur(dungeons, k, 0);
         
-        // 정답
         return answer;
     }
     
-    public void recur(int nowK, int[][] dungeons, int count){
+    public void recur(int[][] dungeons, int nowK, int count){
         for(int i = 0; i<dungeons.length; i++){
             if(!visited[i] && nowK >= dungeons[i][0]){
                 visited[i] = true;
-                recur(nowK-dungeons[i][1], dungeons, count+1);
+                recur(dungeons, nowK - dungeons[i][1], count+1);
                 visited[i] = false;
             }
         }
         answer = Math.max(answer, count);
     }
 }
-
-
-
