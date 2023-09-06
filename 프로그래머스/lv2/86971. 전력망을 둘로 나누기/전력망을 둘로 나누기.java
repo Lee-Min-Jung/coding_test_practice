bf71d1717269eea2a6a6aa1fffea13c2996e95f3
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-09-06 14:34:06 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.74 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,25 +1,20 @@
 // 생각
-    // 일단 하나의 그래프를 만들고, 특정 연결 끊어서 그래프 2개로 만든 다음 bfs나 dfs돌면서 총 순회 개수 구해서 차이 구하기
+    // wires 인접리스트로 만들어서 시작 wire 끊고 bfs 각각 돌아서 차이 계산
 // 구현
-    // for(wires) : 그래프 만들기
-    // for(wires) 
-        // 해당 원소 연결 끊고 각각으로부터 bfs 돌면서 개수 세기
-        // answer = Math.min(|첫번째 원소 결과 - 두번째 원소 결과|, answer);
-        // 연결 다시 복구
-    // bfs(int target)
-        
-    
+    // 인접리스트 만들기
+    // 하나씩 끊으면서 확인
+    // bfs
 import java.util.*;
 
 class Solution {
     static List<Integer>[] adj;
-    static int N;
     public int solution(int n, int[][] wires) {
         // 변수
-        adj = new List[n+1];
         int answer = Integer.MAX_VALUE;
-        N = n;
-        // 연결리스트 만들기
+        
+        
+        // 인접리스트 만들기
+        adj = new List[n+1];
         for(int i = 1; i<=n; i++){
             adj[i] = new ArrayList<Integer>();
         }
@@ -29,39 +24,37 @@ class Solution {
             adj[start].add(end);
             adj[end].add(start);
         }
+      
         
-        // wire 끊어보기
+        // 하나씩 끊으면서 확인
         for(int i = 0; i<wires.length; i++){
             int start = wires[i][0];
             int end = wires[i][1];
-            // 끊기
+            boolean[] visited = new boolean[n+1];
+            
             adj[start].remove(Integer.valueOf(end));
             adj[end].remove(Integer.valueOf(start));
-            answer = Math.min(answer, Math.abs(bfs(start) - bfs(end)));
-            // 다시 연결
+            answer = Math.min(answer, Math.abs(bfs(start, visited)-bfs(end, visited)));
             adj[start].add(end);
             adj[end].add(start);
         }
         
-       return answer;
-        
-        
+        return answer;
     }
     
-    public int bfs(int target){
-        boolean[] visited = new boolean[N+1];
+    // bfs
+    public int bfs(int num, boolean[] visited){
         Queue<Integer> q = new LinkedList<Integer>();
         int count = 0;
-        
-        q.offer(target);
+        q.add(num);
         
         while(!q.isEmpty()){
-            int val = q.poll();
-            visited[val] = true;
+            int cur = q.poll();
+            visited[cur] = true;
             count++;
-            for(int num : adj[val]){
-                if(!visited[num]){
-                    q.offer(num);
+            for(int next : adj[cur]){
+                if(!visited[next]){
+                    q.add(next);
                 }
             }
         }
