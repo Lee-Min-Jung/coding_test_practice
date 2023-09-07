ea70abd13a89d8f23a7867290c267af69284b7d9
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-09-07 13:59:45 +0900

    [level 3] Title: 단어 변환, Time: 0.04 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -1,54 +1,47 @@
-// 생각
-    // words를 탐색해야 함... 
-    // 예전에 계속했던 재귀를 활용한 완전탐색
-// 구현
-    // recur(String[] words, String begin)
-        // if(begin == target) : 종료
-        // for(words)
-            // begin과 words[i]가 하나 차이 && !visited[i]
-                // count++
-                // visited[i] = true
-                // recur(words, words[i])
-                // visited[i] = false;
-
 class Solution {
-    static int answer = Integer.MAX_VALUE;
     static boolean[] visited;
+    static int answer = Integer.MAX_VALUE;
+    
     public int solution(String begin, String target, String[] words) {
+        // 변수
         visited = new boolean[words.length];
         
+        // 재귀
         recur(words, begin, target, 0);
         
-        if(answer == Integer.MAX_VALUE){
-            answer = 0;
+        if(answer != Integer.MAX_VALUE){
+            return answer;
         }
-        
-        return answer;
+        return 0;
+       
     }
     
-    public void recur(String[] words, String begin, String target, int count){
-        
-        if(begin.equals(target)){
-            answer = count;
+    public void recur(String[] words, String word, String target, int count){
+        if(word.equals(target)){
+            answer = Math.min(answer, count);
             return;
         }
         for(int i = 0; i<words.length; i++){
-            if(!visited[i] && compareStr(begin, words[i]) == 1){
-                visited[i] = true;
-                recur(words, words[i], target, count+1);
-                visited[i] = false;
+            if(!visited[i]){
+                if(onlyOneDiffer(word, words[i])){
+                    visited[i] = true;
+                    recur(words, words[i], target, count+1);
+                    visited[i] = false;
+                }
             }
         }
-        
     }
     
-    public int compareStr(String str1, String str2){
+    public boolean onlyOneDiffer(String str1, String str2){
         int count = 0;
         for(int i = 0; i<str1.length(); i++){
             if(str1.charAt(i) != str2.charAt(i)){
                 count++;
             }
         }
-        return count;
+        if(count == 1){
+            return true;
+        }
+        return false;
     }
 }
