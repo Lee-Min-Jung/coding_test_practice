e168706e491aa553a49c54e31f3eba168c1d1195
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-09-08 14:17:51 +0900

    [level 1] Title: K번째수, Time: 0.62 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,21 +1,29 @@
+// 생각
+    // commands를 돌면서 각 인덱스 보고 array에서 값 구하기
+// 구현
+    // for(commands)
+        // i, j, k 추출
+        // for(i-1~j-1)
+            // array[i-1] 부터 array[j-1] 까지의 값 추출
+        // 새로 구한 배열 정렬
+        // 새로 구한 배열에서 k-1번째 값 저장
 import java.util.*;
- 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        // commands를 돌면서 범위와 인덱스를 구해서 array에 적용하여 값 하나 얻은 후 추가
         int[] answer = new int[commands.length];
-        
         for(int i = 0; i<commands.length; i++){
-            int start = commands[i][0]-1;
-            int end = commands[i][1];
-            int index = commands[i][2]-1;
-            int[] slice_arr = Arrays.copyOfRange(array, start, end);
-            Arrays.sort(slice_arr);
-            answer[i] = slice_arr[index];
+            int startIndex = commands[i][0]-1;
+            int endIndex = commands[i][1]-1;
+            int k = commands[i][2]-1;
+            int[] subArr = new int[endIndex-startIndex+1];
+            int m = 0;
+            for(int j = startIndex; j<=endIndex; j++){
+                subArr[m] = array[j];
+                m++;
+            }
+            Arrays.sort(subArr);
+            answer[i] = subArr[k];
         }
-        
-        
-        
         return answer;
     }
 }
