52cfa98ac4625fff1b8819b8b8087d619b0d2592
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-09-08 18:06:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.99 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,33 +1,34 @@
-// participant에는 있는데 completion에는 없는 사람들 구하기
-// participant를 돌기
-    // map에 이름: 1,2... 이런식으로 저장하기
-// completion을 돌기
-    // map에서 이름을 키로 해서 값을 찾고 값에 -1 하기
-// map을 돌기
-    // 각 키의 값이 1인 것이 답
-
+// 생각
+    // map만들어서 참여자 저장 싹 하고 완수자를 map에서 찾으면서 못 완수한 사람 리턴
+// 구현
+    // for(participant)
+        // map에 모두 저장해서 값 1로, 동명이인이면 값 2되게
+    // for(completion)
+        // 해당 값을 map에서 찾아서 값 1씩 빼기
+    // for(map)
+        // 값 0인 거 찾아서 key 리턴
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> participantMap = new HashMap<String, Integer>();
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
         
-        // 참여자 돌면서 map 만들기
-        for(String name : participant){
-            participantMap.put(name, participantMap.getOrDefault(name, 0)+1);
+        // participant 돌기
+        for(int i = 0; i<participant.length; i++){
+            hm.put(participant[i], hm.getOrDefault(participant[i], 0)+1);
         }
         
-        // 완주자 돌기
-        for(String name : completion){
-            participantMap.put(name, participantMap.get(name)-1);
+        
+        // completion 돌기
+        for(int i = 0; i<completion.length; i++){
+            hm.put(completion[i], hm.get(completion[i])-1);
         }
         
-        // map 돌기
+        
+        // map돌기
         String answer = "";
-        for(String key : participantMap.keySet()){
-            if(participantMap.get(key) == 1){
+        for(String key : hm.keySet()){
+            if(hm.get(key) != 0){
                 answer = key;
-                break;
             }
         }
         
