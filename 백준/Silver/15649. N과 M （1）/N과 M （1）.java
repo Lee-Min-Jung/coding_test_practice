77199a49c598000f0c480c1536cc6c682c8242b0
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-09-15 10:20:29 +0900

    [Silver III] Title: N과 M (1), Time: 2192 ms, Memory: 55700 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,34 +1,27 @@
+import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
-
-
-
-
-
 
 public class Main {
     static int N, M;
-    static int[] output;
     static boolean[] visited;
-    public static void main(String[] args) throws IOException {
+    static int[] output;
+
+    public static void main(String[] args) throws IOException{
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
         M = sc.nextInt();
-        output = new int[M];
+
         visited = new boolean[N];
+        output = new int[M];
 
-        backTracking(0);
-
+        bt(0);
     }
 
-    public static void backTracking(int depth){
+    public static void bt(int depth){
         if(depth == M){
             for(int i = 0; i<output.length; i++){
-                System.out.print(output[i] + " ");
+                System.out.print(output[i] +" ");
             }
             System.out.println();
             return;
@@ -37,19 +30,9 @@ public class Main {
             if(!visited[i]){
                 visited[i] = true;
                 output[depth] = i+1;
-                backTracking(depth+1);
+                bt(depth+1);
                 visited[i] = false;
             }
         }
     }
-
-
-
-
-
-
 }
-
-
-
-
