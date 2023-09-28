17e185f34b313d9af5bc0f7cf2c0270adf00be73
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-09-28 18:52:57 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,8 +1,13 @@
-# 보호소에 들어올 당시에는 중성화되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회
-SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME 
+# 들어올 당시에는 중성화 되지 않았지만 보호소 나갈 때는 중성화된 동물 구하기
+# 아이디, 생물종, 이름 / 아이디 오름차순
+
+SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
 FROM ANIMAL_INS I
-INNER JOIN ANIMAL_OUTS O
+
+LEFT JOIN ANIMAL_OUTS O
 ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE LEFT(I.SEX_UPON_INTAKE, 6) = 'Intact' 
-AND(LEFT(O.SEX_UPON_OUTCOME, 6) = 'Spayed' OR LEFT(O.SEX_UPON_OUTCOME, 8) = 'Neutered')
+
+WHERE I.SEX_UPON_INTAKE LIKE 'Intact%' 
+AND (O.SEX_UPON_OUTCOME LIKE 'Spayed%' OR O.SEX_UPON_OUTCOME LIKE 'Neutered%')
+
 ORDER BY I.ANIMAL_ID
