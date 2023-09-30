0e192fd89bf7dec39e422b2fb9631f942349fa41
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-09-30 22:27:11 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,14 @@
+# 중고거래 게시물을 3건 이상 등록한 사용자 구하기
+# 사용자 id, 닉네임, 전체주소(상세도 합쳐), 전화번호(-사용) / 회원 ID 내림차순
+
+SELECT U.USER_ID, U.NICKNAME, CONCAT(U.CITY, " ", U.STREET_ADDRESS1, " ", U.STREET_ADDRESS2) AS '전체주소', 
+        CONCAT(SUBSTR(U.TLNO, 1, 3) , '-', SUBSTR(U.TLNO, 4, 4), '-', SUBSTR(U.TLNO, 8, 4)) AS '전화번호'
+FROM USED_GOODS_USER U
+
+LEFT JOIN USED_GOODS_BOARD B
+ON U.USER_ID = B.WRITER_ID
+
+GROUP BY U.USER_ID
+HAVING COUNT(U.USER_ID) >= 3
+
+ORDER BY U.USER_ID DESC
