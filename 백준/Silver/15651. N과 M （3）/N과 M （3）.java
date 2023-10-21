a154c10dbc979d2e9d6f2603a6b0dd1bc6778537
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2023-10-21 10:48:02 +0900

    [Silver III] Title: N과 M (3), Time: 452 ms, Memory: 67976 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,37 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.util.*;
-import java.io.*;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
     static int N, M;
     static int[] output;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         output = new int[M];
 
         bt(0);
 
-        System.out.print(sb);
-    }
+        System.out.println(sb);
 
-    public static void bt(int depth){
+    }
+    static void bt(int depth){
         if(depth == M){
-            for(int i = 0; i<output.length; i++){
-                sb.append(output[i] + " ");
+            for(int i = 0; i<M; i++){
+                sb.append(output[i]);
+                sb.append(" ");
             }
-            sb.append('\n');
+            sb.append("\n");
             return;
         }
         for(int i = 0; i<N; i++){
             output[depth] = i+1;
             bt(depth+1);
         }
-
-
     }
+
+
+
+
 }
+
