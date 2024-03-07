7b219c1f926c6d9d804b9f9ba3ca1d007b5699f8
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2024-03-07 22:32:04 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
@@ -1,52 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.StringTokenizer;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static List<Integer>[] cowList;
-    static int answer = 0;
+
     public static void main(String[] args) throws IOException {
+        // 입력 준비
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 입력 및 초기화
-        N = Integer.parseInt(st.nextToken());
-        cowList = new List[11];
-        for(int i = 1; i<=10; i++){
-            cowList[i] = new ArrayList<>();
-        }
+        // 관찰 횟수
+        int N = Integer.parseInt(st.nextToken());
 
-        // 소 번호와 위치 입력
+        // 소 번호별 위치 저장
+        List<Integer>[] cowNumList = new List[11];
+        for(int i = 1; i<=10; i++){
+            cowNumList[i] = new ArrayList<>();
+        }
         for(int i = 0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int cowNum = Integer.parseInt(st.nextToken());
             int position = Integer.parseInt(st.nextToken());
-            cowList[cowNum].add(position);
+            cowNumList[cowNum].add(position);
         }
 
-        // 소 리스트 돌면서 위치 변화 카운트
+        // 소 번호별 위치 돌면서 위치 변화 카운트
+        int answer = 0;
         for(int i = 1; i<=10; i++){
-            if(cowList[i].isEmpty()) continue;
-            int cur = cowList[i].get(0);
-            for(int position : cowList[i]){
-                if(cur != position){
-                    answer++;
-                }
+            if(cowNumList[i].isEmpty()) continue;
+            int cur = cowNumList[i].get(0);
+            for(int position : cowNumList[i]){
+                if(cur != position) answer++;
                 cur = position;
             }
         }
 
         System.out.println(answer);
+
+
+
+
     }
 
 
 
-
-
-
-
 }
+
+
 
