13cc2b35130cccc406e9c5637ff4f74812a138fa
이민정 <82032462+Lee-Min-Jung@users.noreply.github.com>
2024-03-17 16:32:39 +0900

    [Gold V] Title: 달력, Time: 172 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.java
+++ 백준/Gold/20207. 달력/달력.java
@@ -1,116 +1,114 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.StringTokenizer;
 import java.util.*;
 
 public class Main {
     static int N;
-    static Schedule[] sch;
-    static int[][] sticker = new int[1001][366];
-    static int[] height = new int[366];
+    static int[][] plan;
+    static int[][] map = new int[1001][366];
+    static int[] height = new int[366]; // 각 열별 스티커 붙여야 할 최고 높이를 담고 있는 배열
     static int answer = 0;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 입력 및 초기화
-        N = Integer.parseInt(st.nextToken());
-        sch = new Schedule[N];
+    N = Integer.parseInt(st.nextToken());
+    plan = new int[N][2];
 
-        // 일정 입력 받기
-        for(int i = 0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            sch[i] = new Schedule(s, e);
-        }
-
-        // 일정 정렬
-        Arrays.sort(sch);
-
-        // 일정 보고 스티커 붙이기
-        putSticker();
-
-        // test
-//        for(int i = 1; i<=1000; i++){
-//            for(int j = 1; j<=365; j++){
-//                System.out.print(sticker[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-
-        // 면적 구하기
-        getArea();
-
-        System.out.println(answer);
-
-
-
+    // 일정 입력
+    for(int i = 0; i<N; i++){
+        st = new StringTokenizer(br.readLine());
+        plan[i][0] = Integer.parseInt(st.nextToken());
+        plan[i][1] = Integer.parseInt(st.nextToken());
     }
+
+    // 일정 정렬
+    Arrays.sort(plan, new Comparator<int[]>() {
+        @Override
+        public int compare(int[] o1, int[] o2) {
+            if(o1[0] == o2[0]) {
+                int period1 = o1[1] - o1[0];
+                int period2 = o2[1] - o2[1];
+                return period2 - period1;
+            }
+            return o1[0] - o2[0];
+        }
+    });
+
+
+
+    // 달력에 일정 채우기
+    for(int i = 0; i<plan.length; i++){
+        int start = plan[i][0];
+        int end = plan[i][1];
+        for(int j = 1; j<=1000; j++){
+            if(map[j][start] == 0){
+                for(int k = start; k<=end; k++){
+                    map[j][k] = 1;
+                }
+                break;
+            }
+        }
+    }
+
     // 코팅지 면적 구하기
-    static void getArea(){
-        // 높이 구하기
-        for(int i = 1; i<=365; i++){
-            int max = 0;
-            for(int j = 1; j<=1000; j++){
-                if(sticker[j][i] != 0){
-                    max = Math.max(j, max);
-                }
-            }
-            height[i] = max;
+    // 각 열 별로 높이를 구해서 저장한다
+    for(int i = 1; i<=365; i++){
+        int max = 0;
+        for(int j = 1; j<=1000; j++){
+            if(map[j][i] != 0) max = Math.max(j, max);
         }
+        height[i] = max;
+    }
 
-        // 넓이 구하기
-        int w = 0;
-        int h = 0;
-        for(int i = 1; i<=365; i++){
-            if(height[i] != 0){
-                w++;
-                h = Math.max(h, height[i]);
-            }
-            else if(height[i] == 0){
-                answer += w*h;
-                w = 0;
-                h = 0;
-            }
+    int w = 0;
+    int h = 0;
+    // 열을 돌면서 높이와 너비를 구한다
+    for(int i = 1; i<=365; i++){
+        if(height[i] != 0){
+            w++;
+            h = Math.max(h, height[i]);
         }
-        if(height[365] != 0){
+        // 높이가 0이라는 건 연속된 게 끝났다는 것이니 지금까지의 면적을 더하고 높이와 너비 리셋이 필요하다
+        else if(height[i] == 0){
             answer += w*h;
+            w = 0;
+            h = 0;
         }
+    }
+    if(height[365] != 0){
+        answer += w*h;
+    }
+    
+    System.out.println(answer);
+
 
     }
-    // 스티커 붙이기
-    static void putSticker(){
-        for(int i = 0; i<N; i++){
-            int startDay = sch[i].startDay;
-            int endDay = sch[i].endDay;
-            for(int j = 1; j<=1000; j++){
-                // 비어 있다
-                if(sticker[j][startDay] == 0){
-                    for(int k = startDay; k<=endDay; k++){
-                        sticker[j][k] = 1;
-                    }
-                    break;
-                }
-            }
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
-// 일정
-class Schedule implements Comparable<Schedule>{
-    int startDay;
-    int endDay;
-    Schedule(int startDay, int endDay){
-        this.startDay = startDay;
-        this.endDay = endDay;
-    }
 
-    public int compareTo(Schedule sc){
-        if(sc.startDay != this.startDay) return this.startDay - sc.startDay;
-        if(sc.endDay - sc.startDay != this.endDay - this.startDay) return (sc.endDay - sc.startDay)-(this.endDay - this.startDay);
-        return this.startDay - sc.startDay;
-    }
-}
+
+
